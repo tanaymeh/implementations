1e0b4c376fec816f362791aa454f05b36b8e839c
Tanay Mehta <tanaymehta28@gmail.com>
2021-05-29 13:49:38 +0000

    Added main running file

--- MLPMixer/main.py
+++ MLPMixer/main.py
@@ -6,5 +6,11 @@ import torch.nn.functional as F
 from .blocks import MLPMixer
 
 model = MLPMixer(
-    image_size=224
+    image_size=224,
+    patch_size=16,
+    tokens_mlp_dim=128,
+    channels_mlp_dim=128,
+    total_classes=2,
+    total_channels=4,
+    total_blocks=4
 )

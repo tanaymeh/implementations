1aba3b7e14c033cf823c408c7c9a353f0d66ffe6
Tanay Mehta <tanaymehta28@gmail.com>
2021-05-29 13:37:50 +0000

    Added all MLP Mixer functions

--- MLPMixer/blocks.py
+++ MLPMixer/blocks.py
@@ -54,3 +54,61 @@ class MixerUnit(nn.Module):
         
         # Return the output from the channel mixer added with original x (another Skip Connection)
         return x + out
+    
+class MLPMixer(nn.Module):
+    def __init__(
+        self, 
+        image_size,
+        patch_size,
+        tokens_mlp_dim,
+        channels_mlp_dim,
+        total_classes,
+        total_channels,
+        total_blocks
+        ):
+        
+        super(MLPMixer, self).__init__()
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.tokens_mlp_dim = tokens_mlp_dim
+        self.channels_mlp_dim = channels_mlp_dim
+        self.total_classes = total_classes
+        self.total_channels = total_channels
+        self.total_blocks = total_blocks
+        
+        assert self.image_size // self.patch_size, \
+            "An integral number of patches should be formed from the image"
+            
+        total_tokens = (self.image_size // self.patch_size) ** 2
+        
+        self.patch_to_embedding = nn.Conv2d(
+            3, 
+            self.total_channels, 
+            kernel_size=self.patch_size, 
+            stride=self.patch_size
+        )
+        
+        self.mixer_blocks = []
+        for i in range(self.total_blocks):
+            self.mixer_blocks.append(MixerUnit(
+                nb_tokens=total_tokens,
+                nb_channels=self.total_channels,
+                token_mlp_dim=self.tokens_mlp_dim,
+                channel_mlp_dim=self.channels_mlp_dim
+            ))
+        
+        self.norm = nn.LayerNorm(self.total_channels)
+        self.head = nn.Linear(self.total_channels, self.total_classes)
+        
+    def forward(self, x):
+        out = self.patch_to_embedding(out)
+        out = einops.rearrange(out, 'n c h w -> n (h w) c')
+        
+        for mixer in self.mixer_blocks:
+            out = mixer(out)
+        
+        out = self.norm(out)
+        out = out.mean(dim=1) # Global average pooling
+        out = self.head(out)
+        
+        return out
